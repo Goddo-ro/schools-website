92367538c067921a20c7043d8feccca2fa3b310b
Goddo-ro <r0manovic1@yandex.ru>
2022-07-08 14:19:04 +0300

    Настроено бургер меню

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,18 +1,22 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 let main = document.querySelector('.main');
-let footerInfo = document.querySelector('.footer-info');
 menuBtn.addEventListener('click', function() {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
     main.classList.toggle('active');
     if (main.classList.contains('active')) {
-        main.style.transform = 'translateX(32%)';
-        footerInfo.style.transform = 'translateX(-35%)'
-        footerInfo.style.width = '118%';
+        main.style.transform = 'translateX(320px)';
     } else {
         main.style.transform = 'translateX(0)';
-        footerInfo.style.transform = 'translateX(-5%)'
-        footerInfo.style.width = '112%';
     }
 })
+
+const title = document.querySelector(".sidenav-title")
+
+window.addEventListener('resize', function(event) {
+    menuBtn.classList.remove('active');
+	menu.classList.remove('active');
+    main.classList.remove('active');
+    main.style.transform = 'translateX(0)';
+}, true);
