3dd7af3660800b4f303362aace43c957c5b21a21
Goddo-ro <r0manovic1@yandex.ru>
2022-07-14 15:54:25 +0300

    секция add services перенесена в отдельный файл для импорта

--- js/includeContent.js
+++ js/includeContent.js
@@ -1,5 +1,6 @@
 $(function(){
     $(".sidenav").load("includes/sidenav.html"); 
+    $(".add-services").load("includes/add_services.html");
     $("header").load("includes/header.html"); 
     $("footer").load("includes/footer.html"); 
 });
