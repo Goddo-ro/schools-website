ad370bbaccaba64b62904400dee9db11f4bb9ea3
Goddo-ro <r0manovic1@yandex.ru>
2022-07-14 16:10:48 +0300

    Скрипт schowSlides вынесен в отдельный файл

---
+++ js/showSlides.js
@@ -0,0 +1,22 @@
+var slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000);
+}
