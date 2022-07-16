ec2465d6c33a0903bdaebea234ff02af49088320
Goddo-ro <r0manovic1@yandex.ru>
2022-07-16 12:30:19 +0300

    Удален вывод в файле изменения активной страницы

--- js/changeActivePage.js
+++ js/changeActivePage.js
@@ -4,8 +4,6 @@ window.addEventListener("load", function(e) {
 
     for (let i = 0; i < links.length; i++) {
         links[i].classList.remove('active-link');
-        console.log(links[i].textContent);
-        console.log(document.title);
         if (links[i].textContent.trim() == this.document.title) {
             links[i].classList.add('active-link');
         }
