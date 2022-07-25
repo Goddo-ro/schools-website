02cc7c2ae88914841c7063d7cc7d9123c02dea3d
Goddo-ro <r0manovic1@yandex.ru>
2022-07-25 11:58:07 +0300

    Исправлен баг, ломающий переключатель слайдов

--- js/showSlides.js
+++ js/showSlides.js
@@ -1,22 +1,34 @@
-var slideIndex = 0;
-showSlides();
+var slideIndex = 1;
+showSlides(slideIndex);
+
+var autoplayInterval = setInterval(function() {
+    document.querySelector(".next").click();
+  }, 4000);
+
+function stopAutoplay() {
+  clearInterval(autoplayInterval);
+}
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000);
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
 }
